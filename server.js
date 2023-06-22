c271e7da14cdbe172f987636005e25807a3ac450
Danny Wittig <wittigdaniel.g@gmail.com>
2023-06-21 22:54:04 -0500

    server listen

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 const path = require ('path');
-const PORT = process.env.port || 3001;
+const PORT = server.listen(process.env.port || 3001);
 
 const api = require('./routes/router.js');
 
